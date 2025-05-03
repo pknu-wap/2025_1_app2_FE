82181eed96073e37436d563761627f1fb868c17a
lepitaaar <leekus1005@gmail.com>
2025-05-04 01:53:21 +0900

    chore: firebase 연동

--- app2_client/macos/Flutter/GeneratedPluginRegistrant.swift
+++ app2_client/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,11 +5,13 @@
 import FlutterMacOS
 import Foundation
 
+import firebase_core
 import geolocator_apple
 import google_sign_in_ios
 import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
